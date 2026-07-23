019ca0b98f9f50aa035ef73fc8c4a7654c247b87
moh962096-prodjet <moh962096@gmail.com>
2026-07-23 11:28:26 +0100

    Add locale extraction function from path

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,1 +1,7 @@
 export * from "./router.tsx";
+import { DEFAULT_LOCALE, isLocale } from "../i18n/config";
+
+export function getLocaleFromPath(pathname: string) {
+  const firstSegment = pathname.split("/").filter(Boolean)[0];
+  return isLocale(firstSegment) ? firstSegment : DEFAULT_LOCALE;
+}
